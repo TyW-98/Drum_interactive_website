bbe1a1e45a1c8aefe672cb9090f2bd78cbb18e45
wey1998 <yitwey1998@hotmail.com>
2023-02-23 18:12:45 +0000

    Add string concatenation and string length count practice

--- practice/javascript_basics.js
+++ practice/javascript_basics.js
@@ -1,8 +1,26 @@
-/* Print hello and world in two line */
+// Print hello and world in two line 
 console.log('Hello World!');
 
-/* Ask for user input and log it into variable named userInput */
+// Ask for user input and log it into variable named userInput 
 var userInput = prompt('What is your name?');
 console.log(userInput);
 alert('Hello'+ userInput);
 
+// String Concatenation 
+var a = "Hello";
+var b = "World";
+var c = a + " " + b;
+
+// Get the length of a string 
+a.length
+
+var userInput = prompt('What is your message?');
+var messageMaxLength = 300;
+var messageLength = userInput.length;
+if (messageLength > messageMaxLength) {
+  alert('Your message exceeds the maximum length');
+} else {
+    var remainingCharacters = messageMaxLength - messageLength;
+    alert('Your message is ' + messageLength + ' characters long and you have ' + remainingCharacters + ' remaining characters');
+
+}

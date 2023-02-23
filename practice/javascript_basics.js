e0d3887dfe1e5fdb0076be802c68d023d09135bf
wey1998 <yitwey1998@hotmail.com>
2023-02-23 19:02:00 +0000

    Add basic mathematical operator practice

--- practice/javascript_basics.js
+++ practice/javascript_basics.js
@@ -15,12 +15,44 @@ var c = a + " " + b;
 a.length
 
 var userInput = prompt('What is your message?');
-var messageMaxLength = 300;
+var messageMaxLength = 100;
 var messageLength = userInput.length;
 if (messageLength > messageMaxLength) {
   alert('Your message exceeds the maximum length');
+  var newMessage = userInput.slice(0, messageMaxLength);
+  alert(newMessage + ' Its length is '+ (newMessage.length));
 } else {
     var remainingCharacters = messageMaxLength - messageLength;
     alert('Your message is ' + messageLength + ' characters long and you have ' + remainingCharacters + ' remaining characters');
-
 }
+
+// string slicing
+var name = 'John';
+console.log(name.slice(0,3))
+
+// Uppercase string
+var name = 'John';
+console.log(name.toUpperCase()) ;
+
+// Lowercase string
+var name = 'John';
+console.log(name.toLowerCase()) ;
+
+// Capitalize string
+var userName = prompt('What is your name?');
+var newName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase();
+alert('Hello ' + newName);
+
+// Replace a string
+var name = 'John';
+console.log(name.replace('John', 'Jane')) ;
+
+// Number Increment and Decrement
+var x = 10;
+x ++; // x = x + 1
+x --; // x = x - 1
+
+// Number Increment and Decrement more than one time
+var x = 10;
+x += 2 // x = x + 2
+x -= 2 // x = x - 2

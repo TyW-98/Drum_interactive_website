d3bdb1c4776e95396bb9f07b934fddb569968b1e
wey1998 <yitwey1998@hotmail.com>
2023-02-23 15:28:13 +0000

    Add variable basics practice

--- practice/javascript_basics.js
+++ practice/javascript_basics.js
@@ -1,2 +1,8 @@
 /* Print hello and world in two line */
 console.log('Hello World!');
+
+/* Ask for user input and log it into variable named userInput */
+var userInput = prompt('What is your name?');
+console.log(userInput);
+alert('Hello'+ userInput);
+

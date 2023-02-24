7e6ab73f95b805269c3eb7e01213a8558e28ee8c
wey1998 <yitwey1998@hotmail.com>
2023-02-24 18:04:06 +0000

    Add Fizz Buzz practice by using WHILE loop to call the function

--- practice/javascript_basics.js
+++ practice/javascript_basics.js
@@ -198,3 +198,26 @@ function fizzBuzz() {
   }
   
 fizzBuzz();
+
+// Fizz Buzz While loop practice
+var output = [];
+var x = 1;
+
+function fizzBuzz() {
+
+    while (x < 101) {
+        if (x %5 == 0 && x %3 == 0) {
+            output.push('FizzBuzz');
+          } else if (x %5 == 0) {
+            output.push('Buzz');
+          } else if (x %3 ==0) {
+            output.push('Fizz');
+          } else {
+            output.push(x);
+          }
+        x++;
+    }
+    console.log(output);    
+}
+
+fizzBuzz();

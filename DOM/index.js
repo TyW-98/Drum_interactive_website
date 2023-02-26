635f9c2a315ce7cc68810d4e8e01c57490965641
wey1998 <yitwey1998@hotmail.com>
2023-02-26 22:53:42 +0000

    Selecting HTML elements using javascript practice 1

--- DOM/index.js
+++ DOM/index.js
@@ -22,3 +22,25 @@ document.querySelector('input[type="checkbox"]').click();
 var thirdItem = document.querySelector('ul').lastElementChild;
 thirdItem.innerHTML = 'Instagram page';
 
+/* Select item from a array using getElementsByTagName*/
+var secondItem = document.getElementsByTagName('li')[1];
+secondItem.innerHTML = 'Facebook page';
+secondItem.style.color = 'green';
+
+/* Get number of elements using array*/
+var numberOfItems = document.getElementsByTagName('li').length;
+console.log(numberOfItems);
+
+/* Get all elements by classname (returns an array)*/
+var facebook = document.getElementsByClassName('facebook');
+
+/* Get element by id (ID is unique hence only 1 item)*/
+var button = document.getElementById('btn');
+button.innerHTML = 'Click me';
+
+/* Can also use querySelector to select by class and id*/
+var button = document.querySelector('#btn');
+var facebook = document.querySelector('.facebook');
+
+
+

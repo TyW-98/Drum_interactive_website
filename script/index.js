443e86e9f5ddc489d84bd25663cb5569c98dcc67
wey1998 <yitwey1998@hotmail.com>
2023-04-07 22:04:15 +0100

    Add background image to each button to represent the drum components

--- script/index.js
+++ script/index.js
@@ -1,7 +1,8 @@
 const drumElements = document.querySelectorAll(".drum");
+const tom1 = new Audio("sounds/tom-1.mp3")
 
 for (var i = 0; i < drumElements.length; i++) {
     drumElements[i].addEventListener("click", function () {
-        alert("clicked");
+        tom1.play();
     });
 }
